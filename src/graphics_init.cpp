0079998e391995731910524680461a13bf95b265
Yuri K. Schlesner <yuriks@yuriks.net>
2013-01-07 01:17:56 -0500

    Enable core OpenGL 3.3 profile.

--- src/graphics_init.cpp
+++ src/graphics_init.cpp
@@ -96,6 +96,8 @@ bool initWindow(int width, int height) {
 
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
 	if (!glfwOpenWindow(width, height, 8, 8, 8, 0, 0, 8, GLFW_WINDOW)) {
 		std::cerr << "Couldn't open window.\n";
