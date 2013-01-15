1b5a559e6809dcc23d363385124bf870f7e5ca07
Yuri K. Schlesner <yuriks@yuriks.net>
2013-01-15 01:08:47 -0600

    Renamed text functions and expanded Font flexibility.

--- src/text.cpp
+++ src/text.cpp
@@ -1,33 +1,39 @@
 #include "text.hpp"
 #include "SpriteBuffer.hpp"
 
-int measureTextWidth(const std::string& text, const FontInfo& font) {
-	return text.length() * font.img_w;
+int measureStringWidth(const std::string& text, const FontInfo& font) {
+	return text.length() * font.char_w;
 }
 
-void drawText(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font) {
+void drawString(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font) {
 	Sprite spr;
 	spr.setPos(x, y);
-	spr.setImg(font.img_x, font.img_y, font.img_w, font.img_h);
+	spr.setImg(font.img_x, font.img_y, font.char_w, font.char_h);
 
 	for (char c : text) {
-		spr.img_x = font.img_x + (c - font.first_char) * font.img_w;
+		const int grid_pos = c - font.first_char;
+		const int grid_line = grid_pos / font.grid_w;
+		const int grid_col = grid_pos % font.grid_w;
+		assert(grid_line < font.grid_h);
+
+		spr.img_x = font.img_x + grid_col * font.char_w;
+		spr.img_y = font.img_y + grid_line * font.char_h;
 		buffer.append(spr);
-		spr.x += font.img_w;
+		spr.x += font.char_w;
 	}
 }
 
-void drawText(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font, TextAlignment alignment) {
+void drawString(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font, TextAlignment alignment) {
 	switch (alignment) {
 	case TextAlignment::left:
-		drawText(x, y, text, buffer, font);
+		drawString(x, y, text, buffer, font);
 		break;
 	case TextAlignment::right:
-		drawText(x - measureTextWidth(text, font), y, text, buffer, font);
+		drawString(x - measureStringWidth(text, font), y, text, buffer, font);
 		break;
 	case TextAlignment::center:
 		// The weird position dance is so that it rounds down instead of up.
-		drawText((2*x - measureTextWidth(text, font)) / 2, y, text, buffer, font);
+		drawString((2*x - measureStringWidth(text, font)) / 2, y, text, buffer, font);
 		break;
 	}
 }
--- src/text.hpp
+++ src/text.hpp
@@ -6,11 +6,14 @@ struct SpriteBuffer;
 
 struct FontInfo {
 	char first_char;
+	int char_w;
+	int char_h;
 	int img_x, img_y;
-	int img_w, img_h;
+	int grid_w, grid_h; // In characters
 
-	FontInfo(char first_char, int img_x, int img_y, int img_w, int img_h)
-		: first_char(first_char), img_x(img_x), img_y(img_y), img_w(img_w), img_h(img_h)
+	FontInfo(char first_char, int char_w, int char_h, int img_x, int img_y, int grid_w, int grid_h)
+		: first_char(first_char), char_w(char_w), char_h(char_h),
+		img_x(img_x), img_y(img_y), grid_w(grid_w), grid_h(grid_h)
 	{ }
 };
 
@@ -18,6 +21,6 @@ enum class TextAlignment {
 	left, right, center
 };
 
-int measureTextWidth(const std::string& text, const FontInfo& font);
-void drawText(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font);
-void drawText(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font, TextAlignment alignment);
+int measureStringWidth(const std::string& text, const FontInfo& font);
+void drawString(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font);
+void drawString(int x, int y, const std::string& text, SpriteBuffer& buffer, const FontInfo& font, TextAlignment alignment);
