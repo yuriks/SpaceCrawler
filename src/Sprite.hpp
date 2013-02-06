e137be5facf38229d7ab6e5d343c5fa2bc48b8a9
Yuri K. Schlesner <yuriks@yuriks.net>
2013-02-06 00:30:53 -0600

    Moved several struct defs. from SpriteBuffer.hpp to Sprite.hpp

---
+++ src/Sprite.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <array>
+#include "vec2.hpp"
+#include <cstdint>
+
+typedef std::array<uint8_t, 4> Color;
+inline Color makeColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+	Color c = {{r, g, b, a}};
+	return c;
+}
+
+static const Color color_white = {{255, 255, 255, 255}};
+
+struct IntRect {
+	int x, y;
+	int h, w;
+};
+
+struct Sprite {
+	int x, y;
+	IntRect img;
+	Color color;
+
+	Sprite() {
+		const Color white = {255, 255, 255, 255};
+		color = white;
+	}
+
+	void setImg(int x, int y, int w, int h) {
+		img.x = x; img.y = y;
+		img.w = w; img.h = h;
+	}
+
+	void setPos(int x_, int y_) {
+		x = x_; y = y_;
+	}
+
+	void setPos(const vec2 v) {
+		x = static_cast<int>(v.x);
+		y = static_cast<int>(v.y);
+	}
+};
+
--- src/SpriteBuffer.hpp
+++ src/SpriteBuffer.hpp
@@ -1,12 +1,12 @@
 #pragma once
+
 #include "gl/gl_1_5.h"
 #include <vector>
-#include <cstdint>
 #include <array>
 #include "graphics_init.hpp"
 #include "texture.hpp"
 #include "gl/Buffer.hpp"
-#include "vec2.hpp"
+#include "Sprite.hpp"
 
 struct VertexData {
 	GLfloat pos_x, pos_y;
@@ -14,44 +14,6 @@ struct VertexData {
 	std::array<GLubyte, 4> color;
 
 	static void setupVertexAttribs();
-};
-
-typedef std::array<uint8_t, 4> Color;
-inline Color makeColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
-	Color c = {{r, g, b, a}};
-	return c;
-}
-
-static const Color color_white = {{255, 255, 255, 255}};
-
-struct IntRect {
-	int x, y;
-	int h, w;
-};
-
-struct Sprite {
-	int x, y;
-	IntRect img;
-	Color color;
-
-	Sprite() {
-		const Color white = {255, 255, 255, 255};
-		color = white;
-	}
-
-	void setImg(int x, int y, int w, int h) {
-		img.x = x; img.y = y;
-		img.w = w; img.h = h;
-	}
-
-	void setPos(int x_, int y_) {
-		x = x_; y = y_;
-	}
-
-	void setPos(const vec2 v) {
-		x = static_cast<int>(v.x);
-		y = static_cast<int>(v.y);
-	}
 };
 
 struct SpriteMatrix {
